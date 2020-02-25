ddcd1507c3e119578813156973e41af7db9c895d
Russell Gordon <github@russellgordon.ca>
2020-02-25 16:06:07 -0500

    Moved background music startup into function for better organization.

--- SpriteKitTemplate/GameScene.swift
+++ SpriteKitTemplate/GameScene.swift
@@ -22,20 +22,9 @@ class GameScene: SKScene {
         // See: http://colorizer.org to find color values
         self.backgroundColor = NSColor(hue: 102/360, saturation: 86/100, brightness: 24/100, alpha: 1)
         
-        // Get a reference to the mp3 file in the app bundle
-        let backgroundMusicFilePath = Bundle.main.path(forResource: "background.wav", ofType: nil)!
-        
-        // Convert the file path string to a URL (Uniform Resource Locator)
-        let backgroundMusicFileURL = URL(fileURLWithPath: backgroundMusicFilePath)
-        
-        // Attempt to open and play the file at the given URL
-        do {
-            backgroundMusic = try AVAudioPlayer(contentsOf: backgroundMusicFileURL)
-            backgroundMusic?.play()
-        } catch {
-            // Do nothing if the sound file could not be played
-        }
-    
+        // Play background music
+        playBackgroundMusic()
+            
     }
         
     func touchDown(atPoint pos : CGPoint) {
@@ -74,6 +63,24 @@ class GameScene: SKScene {
     // Avoid putting computationally intense code in this function to maintain high performance
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func playBackgroundMusic() {
+        
+        // Get a reference to the mp3 file in the app bundle
+        let backgroundMusicFilePath = Bundle.main.path(forResource: "background.wav", ofType: nil)!
+        
+        // Convert the file path string to a URL (Uniform Resource Locator)
+        let backgroundMusicFileURL = URL(fileURLWithPath: backgroundMusicFilePath)
+        
+        // Attempt to open and play the file at the given URL
+        do {
+            backgroundMusic = try AVAudioPlayer(contentsOf: backgroundMusicFileURL)
+            backgroundMusic?.play()
+        } catch {
+            // Do nothing if the sound file could not be played
+        }
+
     }
 
 }
